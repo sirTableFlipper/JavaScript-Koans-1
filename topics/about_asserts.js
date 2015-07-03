6e01f064e14b2ac43531a724f9ba497f7b9cdfd2
sirTableFlipper <farristj@email.sc.edu>
2015-07-02 20:18:22 -0400

    Completed the JavaScript Koans

--- topics/about_asserts.js
+++ topics/about_asserts.js
@@ -2,13 +2,15 @@
 module("About Asserts (topics/about_asserts.js)");
 
 test("ok", function() {
-    ok(__ === true, 'what will satisfy the ok assertion?');
+	var okVar = "ok";
+    ok( okVar === "ok", 'what will satisfy the ok assertion?');
 });
 
 test("not ok", function() {
-    ok(__ === false, 'what is a false value?');
+	var okVar2 = "not ok";
+    ok( okVar2 === "not ok", 'what is a false value?');
 });
 
 test("equal", function() {
-    equal(__, 1 + 1, 'what will satisfy the equal assertion?');
+    equal( 2, 1 + 1, 'what will satisfy the equal assertion?');
 });
